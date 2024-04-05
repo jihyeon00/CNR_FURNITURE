5466432c0263194fcc879d0f1a618bed5a65ced9
이시연 <sieoh@naver.com>
2024-04-05 21:24:49 +0900

    240405 siyeon04: memberInfo.jsp 완수, memberRole.jsp 권한 옵션유지까지 진행

--- CNR_FURNITURE/src/main/java/com/cnr_furniture/controller/MemberController.java
+++ CNR_FURNITURE/src/main/java/com/cnr_furniture/controller/MemberController.java
@@ -1,12 +1,21 @@
 package com.cnr_furniture.controller;
 
+import com.cnr_furniture.domain.member.MemberVO;
+import com.cnr_furniture.domain.member.Search;
+import com.cnr_furniture.service.MemberService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import lombok.extern.log4j.Log4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Controller
 @Log4j
 public class MemberController {
+  public MemberService memberService;
 
   /**
    * Desc: 로그인 페이지
@@ -14,6 +23,7 @@ public class MemberController {
    */
   @GetMapping("/login")
   public String login() {
+
     return "member/login";
   }
 
@@ -21,7 +31,7 @@ public class MemberController {
    * Desc: 비밀번호변경 페이지
    * @return 비밀번호변경 view page
    */
-  @GetMapping("/pwChange")
+  @GetMapping("/PasswordChange")
   public String pwChange() {
 
 
@@ -29,12 +39,49 @@ public class MemberController {
   }
 
   /**
+   * Desc: 사원정보조회
+   * @return 사원정보조회 view page
+   */
+  @GetMapping("/memberInfo")
+  public String memberInfo(Search search, Model model) {
+    // 검색창
+    model.addAttribute("search", search);
+
+    // 사원리스트
+    List<MemberVO> staffList = memberService.staffList(search);
+    model.addAttribute("StaffList", staffList);
+    log.info(staffList);
+
+    // 매니저리스트›
+    List<MemberVO> managerList = memberService.managerList(search);
+    model.addAttribute("ManagerList", managerList);
+
+    // 디렉터 리스트
+    List<MemberVO> directorList = memberService.directorList(search);
+    model.addAttribute("directorList", directorList);
+
+    return "member/memberInfo";
+  }
+
+  /**
    * Desc: 사용자별 권한관리
    * @return 사용자별 권한관리 view page
    */
   @GetMapping("/memberRole")
-  public String memberRole() {
+  public String memberRole(Search search, Model model) {
+    // 검색창
+    model.addAttribute("search", search);
 
+    // 직원 목록
+    List<MemberVO> employeeList = memberService.employeeList(search);
+    model.addAttribute("EmployeeList", employeeList);
+
+    // 권한 조회
+    List<MemberVO> roleList = memberService.roleList();
+    model.addAttribute("roleList", roleList);
+    // 부서 조회
+    List<MemberVO> dpNameList = memberService.dpNameList();
+    model.addAttribute("dpNameList", dpNameList);
 
     return "member/memberRole";
   }
--- CNR_FURNITURE/src/main/java/com/cnr_furniture/mapper/MemberMapper.java
+++ CNR_FURNITURE/src/main/java/com/cnr_furniture/mapper/MemberMapper.java
@@ -1,5 +1,26 @@
 package com.cnr_furniture.mapper;
 
+import com.cnr_furniture.domain.member.MemberVO;
+import com.cnr_furniture.domain.member.Search;
+import java.util.List;
+
 public interface MemberMapper {
+  /** 사원정보 조회 **/
+  MemberVO selectMember(String E_EMAIL);
+
+  /** 직원목록 조회 **/
+  List<MemberVO> employeeList(Search search);
+  /** 사원 리스트 조회 **/
+  List<MemberVO> staffList(Search search);
+  /** 매니저 리스트 조회 **/
+  List<MemberVO> managerList(Search search);
+  /** 디렉터 리스트 조회 **/
+  List<MemberVO> directorList(Search search);
+
+
+  /** 권한 조회 **/
+  List<MemberVO> roleList();
+  /** 부서 조회 **/
+  List<MemberVO> dpNameList();
 
 }
